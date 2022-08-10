2db48a8d846bdfffee488985338409b75dbaeee0
Amr Yousry <amryousry19@gmail.com>
2022-08-10 12:46:02 -0400

    Syntax Sugar

---
+++ Syntax_Sugar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Syntax_Sugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+
+            string response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+
+            Console.WriteLine(response);
+        }
+    }
+}
